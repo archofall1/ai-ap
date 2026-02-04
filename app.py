e72df5b03a238e8f564a06f0f1eb2d809c66980e
archofall1 <darshgajjar4484@gmail.com>
2026-02-04 18:18:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.markdown("---")
 # 2. Secret Key Setup (This pulls from Streamlit's settings)
 try:
     api_key = st.secrets["HF_TOKEN"]
-    client = InferenceClient("HuggingFaceH4/zephyr-7b-beta", token=api_key)
+    client = InferenceClient("meta-llama/Llama-3.2-1B-Instruct", token=api_key)
 except Exception:
     st.error("Missing API Key! Please add HF_TOKEN to your Streamlit Secrets.")
     st.stop()
